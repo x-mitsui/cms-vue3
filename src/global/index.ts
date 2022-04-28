5b9e97474f8a2dc7a019709dfc8f94755e03a13b
dzq <x_mitsui@163.com>
2022-04-28 16:56:01 +0800

    feat(project config): element-plus 配置

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,1 +1,17 @@
-import axios from "axios";
+import { ElButton, ElIcon, ElTabPane, ElTabs, ElTag } from 'element-plus'
+import { App } from 'vue'
+import { UserFilled, Cellphone } from '@element-plus/icons-vue'
+const ElComponents = [
+  ElButton,
+  ElTag,
+  ElTabs,
+  ElTabPane,
+  ElIcon,
+  UserFilled,
+  Cellphone
+]
+export default function (app: App): void {
+  ElComponents.forEach((elCpn) => {
+    app.component(elCpn.name, elCpn)
+  })
+}
