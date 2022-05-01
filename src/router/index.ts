cc85ff63506f5a41cc2a23fc678780d7cbdc612c
dzq <x_mitsui@163.com>
2022-05-01 21:52:06 +0800

    fix: createWebHashHistory报错不知道为什么,换成hash了

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 // type指明引入的只是类型，不加也可以
 import type { RouteRecordRaw } from 'vue-router'
 import localCache from '@/utils/cache'
@@ -27,7 +27,7 @@ const routes: RouteRecordRaw[] = [
 ]
 const router = createRouter({
   routes,
-  history: createWebHistory()
+  history: createWebHashHistory()
 })
 
 router.beforeEach((to, from, next) => {
@@ -42,6 +42,18 @@ router.beforeEach((to, from, next) => {
     } else {
       next('/login')
     }
+  }
+})
+
+/* 路由异常错误处理，尝试解析一个异步组件时发生错误，重新渲染目标页面 */
+router.onError((error) => {
+  const pattern = /Loading chunk (\d)+ failed/g
+  const isChunkLoadFailed = error.message.match(pattern)
+  if (isChunkLoadFailed) {
+    // 用路由的replace方法，并没有相当于F5刷新页面，失败的js文件并没有从新请求，会导致一直尝试replace页面导致死循环，而用 location.reload 方法，相当于触发F5刷新页面，虽然用户体验上来说会有刷新加载察觉，但不会导致页面卡死及死循环，从而曲线救国解决该问题
+    location.reload()
+    // const targetPath = $router.history.pending.fullPath;
+    // $router.replace(targetPath);
   }
 })
 
